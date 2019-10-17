16b150722094dcb5654ba17278d619a48923eb9e
Joven Peralta <jperalta@lcs.on.ca>
2019-10-16 20:10:56 -0400

    Initial Commit

---
+++ AreaHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Peralta, Joven on 2019-10-16.
+//  Copyright © 2019 Peralta, Joven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
