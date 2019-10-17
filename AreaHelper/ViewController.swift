c1a0dd6579738c028e9f897555cc19b37dd475b6
Joven Peralta <jperalta@lcs.on.ca>
2019-10-16 21:13:06 -0400

    Connected the area of the rectangle view controller to the code

--- AreaHelper/ViewController.swift
+++ AreaHelper/ViewController.swift
@@ -10,17 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    func areaOfRectangle (length: Double, width: Double) -> Double {
-        return length * width
-    }
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func areaOfRectangle (length: Double, width: Double) -> Double {
+        return length * width
+    }
+    
+    let lengthInput = UITextView()
+    let widthInput = UITextView()
 
 }
 
