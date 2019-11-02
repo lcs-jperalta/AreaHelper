9cbc7d1119c0d5c97fae05b8dc81dcc7f42133f6
Joven Peralta <jperalta@lcs.on.ca>
2019-11-02 17:06:12 -0400

    Fixed the code so it would find the area for a circle.

--- AreaHelper/CircleViewController.swift
+++ AreaHelper/CircleViewController.swift
@@ -10,8 +10,7 @@ import UIKit
 
 class CircleViewController: UIViewController {
     // MARK: Properties
-    @IBOutlet weak var submittedLength: UITextField!
-    @IBOutlet weak var submittedWidth: UITextField!
+    @IBOutlet weak var submittedRadius: UITextField!
     @IBOutlet weak var resultArea: UILabel!
     
     
@@ -27,39 +26,21 @@ class CircleViewController: UIViewController {
     @IBAction func getArea(_ sender: Any) {
         // Tests if the text field contains an empty string, and if it does, it gives off an error message
         // Converts the number into a string
-        guard let lengthText = submittedLength.text, lengthText != "" else {
+        guard let radiusText = submittedRadius.text, radiusText != "" else {
             resultArea.text = ("Please enter the numbers for length or width.")
             return
         }
-        
         // Tests if the text field contains something other than a number (letters, symbols), and if it does, it gives off an error message
         // Converts the string into a double
-        guard let lengthNumber = Double(lengthText) else {
-            resultArea.text = ("Please enter a number.")
-            return
-        }
-        
-        // Tests if the text field contains an empty string, and if it does, it gives off an error message
-        // Converts the number into a string
-        guard let widthText = submittedWidth.text, lengthText != "" else {
-            resultArea.text = ("Please enter the numbers for length or width.")
-            return
-        }
-        
-        // Tests if the text field contains something other than a number (letters, symbols), and if it does, it gives off an error message
-        // Converts the string into a double
-        guard let widthNumber = Double(widthText) else {
+        guard let radiusNumber = Double(radiusText) else {
             resultArea.text = ("Please enter a number.")
         return
         }
         
-        let areaOfRectangle = widthNumber * lengthNumber
+        let areaOfCircle = radiusNumber * Double.pi
         
         // Show the total area of the rectangle
-        resultArea.text = String(areaOfRectangle)
-        
-        
-        
+        resultArea.text = String(areaOfCircle)
         
     }
     
